2123b2a93c2a7e1733bda52fd35c040a5d464889
Am <arphasirisubb@gmail.com>
2023-11-26 18:21:42 +0700

    Submit objects assignment

--- ex-3.js
+++ ex-3.js
@@ -4,26 +4,15 @@
 const employee = {
   name: "John Doe",
   age: 18,
-  address: {
-    street: "123 Main Street",
-    city: "Cityville",
-    states: "Stateville",
-    country: "Countryland",
-    postalCode: "12345",
-  },
+};
+let address = {
+  street: "123 Main Street",
+  city: "Cityville",
+  states: "Stateville",
+  country: "Countryland",
+  postalCode: "12345",
 };
 
-console.log("Employee Name: " + employees.name);
-console.log("Employee Age: " + employee.age);
-console.log(
-  "Employee Address: " +
-    employee.address.street +
-    ", " +
-    employee.address.city +
-    ", " +
-    employee.address.state +
-    ", " +
-    employee.address.country +
-    " " +
-    employee.adress.postalCode
-);
+console.log(employee.name);
+console.log(employee.age);
+console.log(address);
